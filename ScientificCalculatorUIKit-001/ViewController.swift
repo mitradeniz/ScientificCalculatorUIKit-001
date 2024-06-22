143fd5a24c4e719430c8d83488cc879ee34e0fd4
mitradeniz <mitradenizkaratas@gmail.com>
2024-06-22 11:34:07 +0300

    Initial Commit

---
+++ ScientificCalculatorUIKit-001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScientificCalculatorUIKit-001
+//
+//  Created by humanoid on 22.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
